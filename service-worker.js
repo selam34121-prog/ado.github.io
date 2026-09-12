7ccfb4be4ed52d671a65eb2057f304597a142b72
Ado <selam34121@gmail.com>
2026-09-12 03:47:33 +0300

    Reload open clients after app updates

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,16 @@
-const CACHE='ado-v6';
+const CACHE='ado-v7';
 const ASSETS=['./','index.html','manifest.json','css/main.css','css/components.css','css/animations.css','css/themes.css','css/fixes.css','js/app.js','js/mobile.js','js/update.js','js/storage.js','js/ui.js','js/commands.js','js/expenses.js','js/subscriptions.js','js/notes.js','js/tools.js','js/ai.js','js/settings.js','js/browser.js','assets/icon.svg'];
 
 self.addEventListener('install',event=>event.waitUntil(
  caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())
 ));
 
-self.addEventListener('activate',event=>event.waitUntil(
- caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())
-));
+self.addEventListener('activate',event=>event.waitUntil((async()=>{
+ await caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))));
+ await self.clients.claim();
+ const windows=await self.clients.matchAll({type:'window'});
+ await Promise.all(windows.map(client=>client.navigate(client.url)));
+})()));
 
 self.addEventListener('fetch',event=>{
  const request=event.request;
